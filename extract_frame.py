ba4681687ab7bf53d372905a1e7d39d893e4e3aa
matheusprandini <matheusprandini.96@gmail.com>
2021-05-14 20:45:48 -0300

    Applied pep format

--- extract_frame.py
+++ extract_frame.py
@@ -1,39 +1,41 @@
 import os
 
 
-all_videos = ('bkb',
-              'eul',
-              'glyph',
-              'lose',
-              'roshan',
-              'shiva',
-              'shrine',
-              'teamFight',
-              'teleport',
-              'towerDestory')
+all_videos = (
+    'bkb',
+    'eul',
+    'glyph',
+    'lose',
+    'roshan',
+    'shiva',
+    'shrine',
+    'teamFight',
+    'teleport',
+    'towerDestroy'
+)
 
 
 def process_videos(video_list=all_videos):
-    if not os.path.exists('./frames'):
-        os.makedirs('./frames')
-
     for video in video_list:
         if os.path.exists("Events/" + video):
             for i in range(1, 11):
                 video_name = "Events/" + video + '/' + video + '_' + str(i) + '.mp4'
                 print(video_name)
-                destination = './frames/' + video + '_'
+                destination = './frames/' + video + '/' + video + '_'
+                if not os.path.exists('./frames/' + video + '/'):
+                    os.makedirs('./frames/' + video + '/')
                 get_frame(video_name, destination)
 
 
 def get_frame(video, directory):
     """
-    extract frames from videos with given FPS 15
+    extract frames from videos with given FPS
     """
 
     fps = '30'
     dest = video[-5:-4]  # The folder that the frame images will end up in
-    os.system('ffmpeg -i ' + video + ' -vf scale=720:720' + ' -r ' + fps + ' ' + directory + dest + '_%03d.png')
+    os.system('ffmpeg -i ' + video + ' -vf scale=720:720' +
+              ' -r ' + fps + ' ' + directory + dest + '_%03d.png')
 
 
 def main():
